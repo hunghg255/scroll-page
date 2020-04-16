db704063c33384ccf8d69681240f6503d4d0640e
giahungbk <giahung197bg@gmail.com>
2020-04-17 00:59:10 +0700

    Updates

---
+++ precache-manifest.a1193601bc03903e06eb64c52915e342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d485e4c1e6667348cb369da393f716b0",
+    "url": "/scroll-page/index.html"
+  },
+  {
+    "revision": "eae73200e6ff51be5ef8",
+    "url": "/scroll-page/static/css/main.72c47fbb.chunk.css"
+  },
+  {
+    "revision": "c5412dc4437157a6fd57",
+    "url": "/scroll-page/static/js/2.25f5fa73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scroll-page/static/js/2.25f5fa73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae73200e6ff51be5ef8",
+    "url": "/scroll-page/static/js/main.af469e3e.chunk.js"
+  },
+  {
+    "revision": "ece60efe5eeb2b943089",
+    "url": "/scroll-page/static/js/runtime-main.4be85a77.js"
+  }
+]);
