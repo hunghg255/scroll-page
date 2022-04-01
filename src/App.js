da59c8b6e3d2d49c28a78b5871b26f92adaa8e39
hunghg255 <hung.hg@hesperiafreepress.com>
2022-04-01 16:15:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import "./App.css";
-import ScrollPage from "./components/ScrollPage";
+import './App.css';
+import ScrollPage from './components/ScrollPage';
 
 function App() {
   return (
-    <ScrollPage isHorizontalScroll={true} speed={0.06}>
-      <div className="box">DIV 1</div>
-      <div className="box">DIV 2</div>
-      <div className="box">DIV 3</div>
-      <div className="box">DIV 4</div>
+    <ScrollPage isHorizontalScroll={false} speed={0.09}>
+      <div className='box'>DIV 1</div>
+      <div className='box'>DIV 2</div>
+      <div className='box'>DIV 3</div>
+      <div className='box'>DIV 4</div>
     </ScrollPage>
   );
 }
